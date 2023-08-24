66278b274d39e2b6fc5ba40b2318159c78a4be88
Patricia Yumi <pati.yumi@yahoo.com.br>
2023-08-24 10:26:54 -0300

    grpc app

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/patyumi/grpc-example/internal/database"
+	"github.com/patyumi/grpc-example/internal/pb"
+	"github.com/patyumi/grpc-example/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	// para rodar com o evans usar essa porta ou especificar no comando
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
